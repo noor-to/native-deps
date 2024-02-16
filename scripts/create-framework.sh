af8f30e8a4c48952930d7fb359651a44d8f06ecb
Mohamad <mo.rajbi@gmail.com>
2024-02-16 13:55:21 +0330

    Comment models move to framework

--- scripts/create-framework.sh
+++ scripts/create-framework.sh
@@ -52,8 +52,8 @@ mv "${OUT}/include" "${OUT}/${_framework}/Versions/A/Headers"
 # Move licenses to Framework
 mv "${OUT}/licenses" "${OUT}/${_framework}/Versions/A/Resources/Licenses"
 
-# Move models to Framework
-mv "${OUT}/models" "${OUT}/${_framework}/Versions/A/Resources/Models"
+# # Move models to Framework
+# mv "${OUT}/models" "${OUT}/${_framework}/Versions/A/Resources/Models"
 
 # Create required framework symlinks
 ln -s A "${OUT}/${_framework}/Versions/Current"
